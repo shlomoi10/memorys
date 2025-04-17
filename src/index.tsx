2c39b98e143074f9b215d4ccec20b06dff3deb6d
Your Name <your.email@example.com>
2025-04-17 12:05:19 +0300

    feat: תמיכה מלאה ב-RTL, עיצוב בסיסי מודרני, תשתית למעצב מקצועי

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cacheRtl = createCache({ key: 'muirtl', stylisPlugins: [], prepend: true });
+document.body.dir = 'rtl';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CacheProvider value={cacheRtl}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </CacheProvider>
   </React.StrictMode>
 );
 
